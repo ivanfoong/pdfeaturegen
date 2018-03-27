eda971db21a55a6ea67c4bd9e8ee48aa49539908
Ivan Foong <vonze21@gmail.com>
2018-03-27 11:22:45 +0800

    tagging as new version pypi doesn't allow overwriting of previous upload

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 setup(name='pdfeaturegen',
-      version='0.0.1',
+      version='0.0.2',
       description='Supporting functions to generate features in Pandas Dataframe for Machine Learning',
       url='https://github.com/ivanfoong/pdfeaturegen',
       author='Ivan Foong',
